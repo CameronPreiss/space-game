e5f1ac694c8647e3e6e8e7c026c7b0265d4c2f88
lukehdonnelly <lukehdonnelly@gmail.com>
2024-10-11 14:34:39 +1030

    cleaning up

--- Ship.h
+++ Ship.h
@@ -4,32 +4,25 @@
 #ifndef SHIP_H
 #define SHIP_H
 
-class Ship : public SpaceObject{
-
+class Ship : public SpaceObject {
 protected:
-int HealthPoints;
-int Damage;
-int inventory[3];
-int inventoryCount;
-
+  int HealthPoints;
+  int Damage;
+  int inventory[3];
+  int inventoryCount;
 public:
+  Ship();
+  Ship(int HealthPoints, int Damage, int* newInventory, int inventoryCount, std::string name, int* location, int size);
 
-Ship();
-Ship(int HealthPoints, int Damage, int* newInventory, int inventoryCount, std::string name, int* location, int size);
+  virtual void AttackShip(Ship& Enemy, int Damage);
+  virtual void ReceiveDamage(int damageTaken);
+  virtual bool isDestroyed();
 
-virtual void AttackShip(Ship& Enemy, int Damage);
-virtual void ReceiveDamage(int damageTaken);
-virtual bool isDestroyed();
+  virtual int get_health();
+  virtual int get_damage();
 
-virtual int get_health();
-virtual int get_damage();
-
-virtual void set_health(int health);
-virtual void set_damage(int damage);
-
-
-
-
+  virtual void set_health(int health);
+  virtual void set_damage(int damage);
 };
 
 #endif
