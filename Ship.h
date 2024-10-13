70f4dfdb8de40d862933d773f024ac7cac06d374
CameronPreiss <cameron.preiss@student.adelaide.edu.au>
2024-10-13 18:23:40 +1030

    commenting

--- Ship.h
+++ Ship.h
@@ -6,18 +6,28 @@
 
 class Ship : public SpaceObject {
 protected:
+  //attributes of each ship
   int HealthPoints;
   int Damage;
   int inventoryCount;
   int* inventory;
 public:
+  //default constructor and normal constructor
   Ship();
   Ship(int HealthPoints, int Damage, int* newInventory, int inventoryCount, std::string name, int* location, int size);
 
+  //the attack and receive damage functions which serve to reduce the enemy ships health or the combat ship's health during battle
+  //the attack ship function takes the inputs of the enemy ship and the damage it will deal
+  //the receive damage function will take the input of the damage dealt to the combat ship
+  //both take no output
   virtual void AttackShip(Ship& Enemy, int Damage);
   virtual void ReceiveDamage(int damageTaken);
+
+  //the isDestroyed function serves to determine whether the ship has no health left and whether it should be removed from the map
+  //the isDestroyed function takes no inputs, but does return a boolean
   virtual bool isDestroyed();
 
+  //setters and getters
   virtual int get_health();
   virtual int get_damage();
 
@@ -27,13 +37,18 @@ public:
   virtual int get_inventoryCount();
 
   virtual int* get_inventory();
+
+  //adding an item to the ship's inventory taking an input of what the index of the item that will be added is
   virtual void addItem(int index);
 
+  //the interact function (child function of spaceObject)
   void interact() const {};
 
+  //randomise function, which takes further use in the combatShip and cargoShip classes
   virtual void randomise() {};
   void generateName();
 
+  //destructor
   ~Ship();
 };
 
