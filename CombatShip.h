80bf7b241ec979b4151afc4eee30b3c19f23968b
CameronPreiss <cameron.preiss@student.adelaide.edu.au>
2024-10-13 18:18:35 +1030

    commenting

--- CombatShip.h
+++ CombatShip.h
@@ -13,13 +13,19 @@ public:
   CombatShip(int HealthPoints, int Damage, int* inventory, int inventoryCount, std::string name, int* location, int size);
 
   //the combat ship's special attack/defense function, allowing it to attack the enemy ship twice
+  //takes the inputs of the enemy ship (that it will deal damage to), and the damage it will deal
+  //takes no outputs
   void doubleAttack(Ship& Enemy, int Damage);
 
   //the attack and receive damage functions which serve to reduce the enemy ships health or the combat ship's health during battle
+  //the attack ship function takes the inputs of the enemy ship and the damage it will deal
+  //the receive damage function will take the input of the damage dealt to the combat ship
+  //both take no output
   void AttackShip(Ship& Enemy, int Damage) override;
   void ReceiveDamage(int damageTaken) override;
 
   //the isDestroyed function serves to determine whether the ship has no health left and whether it should be removed from the map
+  //the isDestroyed function takes no inputs, but does return a boolean
   bool isDestroyed() override;
 
   //setters and getters
@@ -30,6 +36,7 @@ public:
   void set_damage(int damage) override;
 
   //randomise function which serves to create many random combat ships when the map is generated
+  //takes no inputs or outputs
   void randomise() override;
 };
 
