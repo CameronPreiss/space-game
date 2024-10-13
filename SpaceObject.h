c86c8cb0c905aad439b6592cd6108c1929fc1c93
CameronPreiss <cameron.preiss@student.adelaide.edu.au>
2024-10-13 19:29:01 +1030

    commenting

--- SpaceObject.h
+++ SpaceObject.h
@@ -6,15 +6,23 @@
 
 class SpaceObject{
 private:
+  //attributes
   std::string type;
   int location[2];
   std::string name;
   int size;
 public:
+  //default constructor and normal constructor
   SpaceObject();
   SpaceObject(int* location, std::string name, int size);
+
+  //virtual scanInfo function (will be inherited by future classes to display their attributes to the console)
   virtual void scanInfo() const;
+
+  //once the player scans for all the nearby planets and ships, they can choose to interact with one of these objects
   virtual void interact() const {};
+
+  //setters and getters
   void set_size(int size);
   int get_size() const;
   void set_location(int* location);
